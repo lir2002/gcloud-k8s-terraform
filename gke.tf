ed86f18dbd37a162aabae246c95a75197c3d4420
Ethan Li <Ethan_Li@epam.com>
2023-02-27 10:44:50 +0800

    Nodes number

--- gke.tf
+++ gke.tf
@@ -9,7 +9,7 @@ variable "gke_password" {
 }
 
 variable "gke_num_nodes" {
-  default     = 1
+  default     = 2
   description = "number of gke nodes"
 }
 
@@ -17,12 +17,13 @@ variable "gke_num_nodes" {
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
-  
+  node_locations = ["us-east1-c", "us-east1-b"]
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 2
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
